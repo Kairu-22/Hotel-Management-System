88295ea1cc6c40f937682cc8591b99589423f7bc
Aarushi <aarushisinha2005@gmail.com>
2023-10-27 10:07:13 +0530

    setting urls and views

--- Backend/Hotel_Management_System/hms/views.py
+++ Backend/Hotel_Management_System/hms/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def index (request):
+    return HttpResponse("This is homepage")
+
+def about (request):
+    return HttpResponse("This is about")
+
+def rooms (request):
+    return HttpResponse("This is rooms")
+
+def blogs (request):
+    return HttpResponse("This is blogs")
+
+def offers (request):
+    return HttpResponse("This is offers")
