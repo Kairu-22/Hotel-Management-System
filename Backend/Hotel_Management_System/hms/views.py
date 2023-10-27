f4f0725de384a4eecb3eb16e2d748083d751d90e
Aarushi <aarushisinha2005@gmail.com>
2023-10-27 19:02:06 +0530

    new user signup and database done

--- Backend/Hotel_Management_System/hms/views.py
+++ Backend/Hotel_Management_System/hms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.models import User
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login, get_user_model
 
 
 # Create your views here.
@@ -21,22 +23,39 @@ def offers (request):
 def gallery (request):
     return render(request, 'gallery.html')
 
-def signin (request):
-    if request.method=="POST" :
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username= username, password=password)
-
-        if user is not None:
-            return redirect("/")
-        else:
-            return render(request, "signin.html")
-    
-    return render(request, 'signin.html')
-
 def signup (request):
+    if not request.user.is_anonymous: #works when user is logged in 
+        return redirect("/")
+    if request.method=="POST":
+
+        User = get_user_model()
+        users = User.objects.all()
+        user_list=[]
+        email_list=[]
+        for i in users:
+            user_list.append(i.username)
+            email_list.append(i.email)
+
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+
+        if email in email_list:
+            messages.error(request, 'Email already signed up. Head to login page.')
+            return render(request,'signup.html')
+        
+        if name in user_list:
+            messages.error(request, 'Username already Taken. Please try something else.')
+            return render(request,'signup.html')
+        
+        user = User.objects.create_user(username=name, email=email, first_name=name, password=password)
+        user.save()
+        messages.success(request, f'Your account is created {name}. Head to login page!')
+    
     return render(request, 'signup.html')
+
+def signin (request):
+    return render(request, 'signin.html')
 
 def signout (request):
     return render(request, 'Welcome.html')
