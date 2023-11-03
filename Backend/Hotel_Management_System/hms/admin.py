29d3f009850f8e5b4fb92d09c57554bdf4e59c22
Boora-Raman <143726797+Boora-Raman@users.noreply.github.com>
2023-11-04 00:00:38 +0530

    Update admin.py

--- Backend/Hotel_Management_System/hms/admin.py
+++ Backend/Hotel_Management_System/hms/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from hms.models import Rooms_details, Bookings
+from hms.models import Rooms_details, Bookings,Room
 
 # Register your models here.
 admin.site.register(Rooms_details)
 admin.site.register(Bookings)
+admin.site.register(Room)
